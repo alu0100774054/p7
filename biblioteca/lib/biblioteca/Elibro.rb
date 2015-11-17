f1428ded39aca5fd4da590d253a303ff286eb627
erikbv99421 <erikbv99421@gmail.com>
2015-11-17 11:56:44 +0000

    creada la expectativa para libro electronico

--- biblioteca/lib/biblioteca/Elibro.rb
+++ biblioteca/lib/biblioteca/Elibro.rb
@@ -1,2 +1,10 @@
 require 'biblioteca/version'
 require 'biblioteca/biblioteca'
+
+class Elibro < Libro
+	attr_accessor :url
+	def initialize(titulo, editorial, nEdicion, fecha,url)
+		super(titulo, editorial, nEdicion, fecha,url)
+		@url = url
+	end	
+end
--- biblioteca/spec/Elibro_spec.rb
+++ biblioteca/spec/Elibro_spec.rb
@@ -1,2 +1,16 @@
 require 'spec_helper'
 require 'bib'
+
+describe Elibro do
+	before :each do
+		@Elibro = Elibro.new("titulo", "editorial", "edicion1", "fecha","url")
+	end
+	describe "#new" do
+		it "recibe parametros y crea un objeto tipo Elibro" do
+			@Elibro.should be_an_instance_of Elibro
+		end
+		#it "devuelve el issn" do
+		#	@Elibro.get_issn.should eql "issn"
+		#end
+	end	
+end
